9a9dc2eef88c9b9f1b97fcc4da8165ade396d2f3
Jonathan Sherpa <ngawangt.sherpa777@gmail.com>
2026-08-27 13:02:54 +0545

    Adding view functionality andusing Enumerate

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ def askuser():
     continuous =  input("Do you want to enter tasks (Y/N)?")
     return continuous
 
+def viewtasks():
+    for number, task in enumerate(tasks):
+        print(f"{number+1}. {task}")
+
 continuous = askuser()
 
 #now, we want to make sure we are able to input tasks till there are 'n' number of tasks.
 while continuous.capitalize() == "Y":
     task = input("Enter the tasks")
     addtasks(task)
+    viewtasks()
     #we need to make sure we get local value of continuous again and again from function_askuser
     continuous = askuser()
 
